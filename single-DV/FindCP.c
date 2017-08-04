0f2ddb29aca628710ed2b6aa666cee4596eafa88
Nancy Song <nsong@alumni.princeton.edu>
2017-08-04 13:03:01 -0400

    Add files to single DVCP

---
+++ single-DV/FindCP.c
@@ -0,0 +1,190 @@
+/*************************************************************************
+FindCP.c  -  Change Point Detection Method
+-------------------
+last modified   : Mon Dec 19 2016 (NS)
+email           : hawyang@princeton.edu, nsong@princeton.edu
+*************************************************************************/
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <float.h>
+#include <stdio.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_sf_gamma.h>
+#include <gsl/gsl_sf_erf.h>
+#include "changepoint.h"
+
+#define MAX_X 10.0
+#define DX    0.01
+#define TOL   1.0e-8
+
+// Functions for calculation of critical values
+double C_ac();
+double Vostrikova();
+struct changepoint* AddCPNode();
+
+// Find a change point in traj of timestep delta_t with bounds cpl (left) and cpr (right), 
+// exclusive. Type-I error of alpha and confidence interval of beta. Found change point added 
+// to tree pointed to by cp. ca points to confidence region critical values. Na is length
+// of traj, rc indicates recursive tracing
+int FindCP(struct changepoint **cp, double *traj, double delta_t, int cpl, int cpr, double alpha, 
+	double beta, int * Ncp, double *ca, int Na, int rc ) {
+	int n,i,k,k_max;				//indices to iterate through traj	
+	int LB, RB;                 	//left and right bounds of confidence region
+	int cp2=0, cp1=0, cp_max;   	//largest change point in absolute index
+
+	//Relevant variables for gaussian mean change point detection
+	double kL, nL;												// Double analogs in integers
+	double lmean, lvar, rmean, rvar, wvar, wmean; 				// Calculation of log-likelihood
+	double critical_region;			// Critical value for specified alpha and N
+	double llrt_max=0.0;			// Maximum log-likelihood
+	double *cumsum, *cumsumSq;		// Arrays to store values
+	enum bool dummy_bool = false;	// If applicable, denotes if found CP has been inserted
+	double dim = 2.0;				// Two dimensions for log-likelihood calculation
+
+	// Used in calculation of type of change point
+	double *vlvar, vrvar;
+	double lvar_max, rvar_max, lmean_max, rmean_max, wvar_max; 
+	double mBIC, vBIC, mvBIC;
+	int type = 0;
+	
+	n = cpr - cpl;
+	LB = cpl;
+	RB = cpr;
+	cp_max = 0;
+	if ( n > 1 ) {
+		nL = (double) n;
+
+		// Create workspace
+		cumsum = (double *) malloc( (n+1) * sizeof(double));
+		cumsumSq = (double *) malloc( (n+1) * sizeof(double));
+		llrt = (double *) calloc((n+1),sizeof(double));
+
+		// Calculate the critical region using Horvath's approximation
+		// Note that the dimensions are two since two parameters are considered
+		critical_region = C_ac(alpha, n, dim); 
+			
+		cumsum[0] = 0;
+		cumsumSq[0] = 0;
+		for (i=1; i<n; i++) {
+			// Estimate Gaussian means at different k
+			cumsum[i] = cumsum[i-1] + traj[cpl+i];
+			cumsumSq[i] = cumsumSq[i-1] + (traj[cpl+i]*traj[cpl+i]);
+		}
+
+		// Overall mean
+		wmean = cumsum[n-1]/(nL-1.0);
+		for(i = cpl+1, wvar = 0.0; i < cpr; i++) {
+			// Overall variance
+			wvar += (traj[i] - wmean) * (traj[i] - wmean)/(nL-2.0)/2.0/delta_t;
+
+			// Variance to be used in variance change point classification
+			vlvar[i-cpl] = wvar*(nL-1.0) * 2.0 * delta_t/(i-cpl);
+		}
+
+		for (k=2,k_max=0, lvar = 1.0, rvar = 1.0; k<n-2; k++) {
+			kL = (double) k;
+
+			lmean = cumsum[k]/k;
+			// Estimation of variance
+			lvar = (cumsumSq[k]/k - lmean*lmean)/delta_t/2.0;
+
+			// Can also explicitly calculate variance
+			// lvar = 0.0;
+			// for(i = cpl+1; i <= cpl+k; i++) {
+			// 	lvar = lvar + (traj[i] - lmean) * (traj[i] - lmean)/k/delta_t/2.0;
+			// }
+
+			rmean = (cumsum[n-1] - cumsum[k])/(nL-kL-1.0);	 
+			// Estimation of variance 
+			rvar = ((cumsumSq[n-1] - cumsumSq[k])/(nL-kL-1.0) - rmean*rmean)/delta_t/2.0;
+
+			// Can also explicitly calculate variance
+			// rvar = 0.0;
+			// for(i = cpl+k+1; i < cpr; i++) {
+			// 	rvar = rvar + (traj[i] - rmean) * (traj[i] - rmean)/(nL-kL-1.0)/delta_t/2.0;
+			// }
+
+			lmean = lmean/delta_t;
+			rmean = rmean/delta_t;
+
+			// Traditional Generalized Log-likelihood ratio test
+			llrt[k] = 0.5 * ((nL-1.0)*log(wvar) - kL * log(lvar) - (nL-kL-1.0)* log(rvar));
+
+			if ( llrt[k] > llrt_max ) {	
+				// Find the maximum of the likelihood functions
+				llrt_max = llrt[k];
+				k_max = k;
+				lvar_max= lvar*delta_t*2.0;
+	        	rvar_max = rvar*delta_t*2.0;
+	        	lmean_max = lmean;
+	        	rmean_max = rmean;
+			}
+		}
+
+		if ( llrt_max > critical_region ) {
+			// Find the left-hand bound of critical region
+			k = k_max;
+			while ( (k>1) && (llrt_max - llrt[k-1] < ca[n]) )
+				k--;
+			LB = k;
+
+			// Find the right-hand bound of critical region
+			k = k_max;
+			while ( (k < n-1) && (llrt[k+1]+ca[n]-llrt_max > 0) ) 
+				k++;
+			RB = k;
+			cp_max = cpl + RB;
+
+			// Determine type of change point
+
+			// Calculate right variance with respect to mu_0
+			vrvar = (wvar*delta_t*2.0*(nL-1.0) - vlvar[k_max] * k_max)/(nL-k_max-1.0);
+			// Calculate overall variance
+	      	wvart = (lvar_max*k_max + rvar_max*(nL-k_max-1.0))/(nL-1.0);
+
+	      	/* BIC model selection for type of change point. Can find max of -BIC = 2*LL - penalty
+			mBIC = (1-nL) * log(2*M_PI*wvart)-(nL-1.0) - 3 * log(nL-1);
+			vBIC = -k_max*log(2*M_PI*vlvar[k_max]) -(nL-k_max-1.0)*log(2*M_PI*vrvar) -(nL-1)-3*log(nL-1);
+			mvBIC = -k_max*log(2*M_PI*lvar_max) -(nL-k_max-1.0)*log(2*M_PI*rvar_max) -(nL-1)-4*log(nL-1);
+
+			// Change in mean
+			if(mBIC > vBIC && mBIC > mvBIC)
+				type = 1;
+			// Change in variance
+			else if(vBIC > mBIC && vBIC > mvBIC)
+				type = 2;
+			// Change in mean and variance
+			else if (mvBIC > mBIC && mvBIC > vBIC)
+				type = 3;
+			*/
+
+			// Add change point to current tree
+			*cp = AddCPNode(k_max+cpl, LB+cpl, RB+cpl, *cp, Ncp, &dummy_bool);
+
+			// No recursion for checking change points
+			if(rc==3){
+				/*********************
+				Free up used workspace
+				*********************/
+				// Return index of change point found
+				return k_max+cpl;
+			}
+			// Recursively find change points
+			if((rc==1)||(rc==0)){
+				// Go to the left branch
+				cp1 = FindCP(cp, traj, cpl, LB+cpl, alpha, beta, Ncp, ca, Na, 1);
+				// Go to the right branch
+				cp1 = FindCP(cp, traj, RB+cpl, cpr, alpha, beta, Ncp, ca, Na, 1);
+				if (cp1 > cp_max ) 
+					cp_max = cp1;
+			}
+		}
+		free(llrt);
+		free(vlvar);
+		free(cumsum);
+		free(cumsumSq);
+	}
+	// Return maximum index of change point found
+	return cp_max;
+}
---
+++ single-DV/main.c
@@ -0,0 +1,185 @@
+/*************************************************************************
+main.c:  Change point detection of diffusion and velocity (non-parallelized)
+         Originally written using OpenMPI v1.4.2
+         Arguments are filename, timestep, alpha, beta
+
+filename:   File with Time Series Data
+alpha:      Type-I error
+1-beta:     Confidence Interval is chosen among {0.99, 0.95, 0.9, 0.69}
+*************************************************************************/
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <float.h>
+#include <gsl/gsl_version.h>
+#include "changepoint.h"
+#include "critical_values.h"
+#include <mpi.h>
+#include <math.h>
+
+#define TOL 1.0e-10
+
+int main(int argc, char *argv[])
+{
+	FILE *fpin, *fpout;    // File pointers
+	char in_name[255],  filename[255], *endptr[255], out_name[255];	// Store strings
+	double alpha = 0.075;  // Type-I error, mis-specify transition
+	double beta = 0.075;   // Confidence interval (CI), mis-specify transition
+	double delta_t = 1;    // Time unit between measurements
+	size_t L = 0;          // Total number of data points
+	size_t NA_BASE;        // Maximum length that can be processed before recursion
+	size_t N_ca;           // Total number ca (critical values for confidence interval)
+	size_t ui;             // Dummy index
+	size_t cpl, cpr, int cp1; // FindCP left start, right end, and return value
+	int Ncp = 0, Ncpdlt;   // Change points found, deleted
+	double tmp;            // Temporary variable for input
+	double *traj;          // Time series
+	struct changepoint *cp_root=NULL; // Change point binary tree
+	clock_t time0, time1;  // Variables for timing
+	int h,i,j,k;           // Dummy indices
+	int trace=0;           // Debug flag
+	double *ca=NULL;       // Critical region threshold for confidence interval
+	enum bool done = false; // Boolean for has a change point been added to tree
+
+	int** cpArray = (int**) malloc(sizeof(int*)); // Change points found
+	int** lbArray = (int**) malloc(sizeof(int*)); // Left bounds of confidence interval
+	int** rbArray = (int**) malloc(sizeof(int*)); // Right bounds of confidence interval
+
+	// Get command line arguments
+	if (argc != 5) {
+		// No file name entered in the command line
+		printf( "\nchangepoint %s%s build %s (GSL version %s)\n", CHANGEPOINT_VERSION, PLATFORM, COMPILE_DATE, GSL_VERSION);
+		printf( "Syntax : changepoint filename delta_t alpha beta\n");
+		printf( "Example: changepoint myfile 0.001 0.05 0.95\n");
+		printf( "         produces myfile.cp with type-I error (alpha) of 5%% and\n");
+		printf( "         confidence interval of 0.95 \n" );
+		printf( "BUG    : Please send emails to hawyang-at-princeton.edu\n\n");
+		exit(1);
+	}
+	else if (argc == 5) {
+		strcpy(in_name, argv[1]);
+		strcpy(out_name, argv[1]);
+		delta_t = strtod(argv[2], endptr);
+		alpha = strtod(argv[3], endptr);
+		beta = strtod(argv[4], endptr);
+	}
+	// Print title
+	printf( "\n" );
+	printf( "********************************************************\n");
+	printf( "					Change Point Detection			   	 \n");
+	printf( "********************************************************\n");
+	printf( "Version: %s%s build %s (GSL version %s)\n\n", 
+		   CHANGEPOINT_VERSION, PLATFORM, COMPILE_DATE, GSL_VERSION );					
+	// Read in confidence interval (beta) critical region data
+	if (beta == 0.99) {
+		N_ca = N_ca99;
+		ca = (double *) realloc(ca, N_ca*sizeof(double));
+		for (ui=0; ui<N_ca; ui++)
+			ca[ui] = ca99[ui];
+	}
+	else if (beta == 0.95) {
+		N_ca = N_ca95;
+		ca = (double *) realloc(ca, N_ca*sizeof(double));
+		for (ui=0; ui<N_ca; ui++)
+			ca[ui] = ca95[ui];
+	}
+	else if (beta == 0.9) {
+		N_ca = N_ca90;
+		ca = (double *) realloc(ca, N_ca*sizeof(double));
+		for (ui=0; ui<N_ca; ui++)
+			ca[ui] = ca90[ui];
+	}
+	else if (beta == 0.69) {
+		N_ca = N_ca69;
+		ca = (double *) realloc(ca, N_ca*sizeof(double));
+		for (ui=0; ui<N_ca; ui++)
+			ca[ui] = ca69[ui];
+	}
+	else {
+		printf("Critical region threshold for confidence region of %.2f is not supplied.\n", beta );
+		printf("Available are: 0.01, 0.05, 0.1, and 0.31.\n" );
+		exit(1);
+	}
+	// Set maximum length
+	NA_BASE = (int)N_ca;
+	// Read in data
+	if ( (fpin=fopen(in_name,"r")) == NULL ) {
+			// File does not exist, exit with error
+			printf("File [%s] does not exist. Exiting ...\n", in_name );
+			exit(1);
+	}
+	else {
+		// File exists, start reading in data
+		printf("] Reading in data file ...\n");
+		traj = (double*) malloc(sizeof(double*));
+		traj[0] = 0.0;
+		while (!feof(fpin) ) {
+			fscanf( fpin, "%le", &tmp );
+			L++;
+			traj = (double *) realloc(traj, (L+1)*sizeof(double));
+			traj[L] = tmp;
+		}
+		fclose( fpin );
+		printf("  Total of %lu data points read in.\n", (long unsigned int) L);
+		// Find change points recursively
+		printf("] Finding change points recursively ...\n"); 
+		time0 = clock();
+		if (NA_BASE > L )
+			cp1 = FindCP(&cp_root, traj, delta_t, 0, L+1, alpha, beta, &Ncp, ca, L, 1);
+		else {
+			// If length of trajectory exceeds bounds of critical value
+			cpl = 0;
+			cpr = NA_BASE;
+			done = false;
+			while (!done ) {
+				cp1 = FindCP(&cp_root, traj, delta_t, cpl, cpr, alpha, beta, &Ncp, ca, L, 1);
+				if (cpr < L) {
+					// No change points found
+					if (cp1 == 0 ) 
+						cpl = cpr - NA_OVERLAP;
+					else 
+						cpl = cp1;
+					cpr = cpl + NA_BASE - 1;
+					if (cpr > L) 
+						cpr = L+1;
+					done = false;
+				}
+				else 
+					done = true;
+			}
+		}
+		time1 = clock();
+		printf("  %i change points found. [%.0f s]\n", Ncp, (double)(time1-time0)/CLOCKS_PER_SEC);
+		// Remove spurious change points
+		printf("] Examining %i change points sequentially ...\n",Ncp);
+		time0 = clock();
+		if (Ncp > 0)
+			CheckCP(&cp_root, traj, delta_t, alpha, beta, trace, &Ncpdlt, trace, ca, L);
+		time1 = clock();
+		printf("  %i spurious change points removed. [%.0f s]\n", Ncpdlt, 
+			(double)(time1-time0)/CLOCKS_PER_SEC);
+		Ncp = 0;
+		MakeCPArray(cp_root, &cpArray[0], &lbArray[0], &rbArray[0], &Ncp);
+		// Save results...
+		strcpy(filename, out_name);
+		strcat(filename, ".cp" );
+		printf("  saving file: %s \n", filename);
+		fpout = fopen( filename, "w");
+		for (i = 1; i <= Ncp; i++)
+			fprintf(fpout, "%d %d %d\n", cpArray[0][i], lbArray[0][i], rbArray[0][i]);
+		fclose(fpout);
+		
+		free(traj);
+		free(cpArray[0]);
+		free(lbArray[0]);
+		free(rbArray[0]);
+		free(cpArray);
+		free(lbArray);
+		free(rbArray);
+	}
+}
